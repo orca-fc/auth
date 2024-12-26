0020fdccbc9cfff7d36622145f65f696e1d7964e
jonghyukleee <jh5253krkr@naver.com>
2024-12-26 20:14:42 +0900

    init

---
+++ src/main/kotlin/com/orca/auth/AuthApplication.kt
@@ -0,0 +1,11 @@
+package com.orca.auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthApplication
+
+suspend fun main(args: Array<String>) {
+	runApplication<AuthApplication>(*args)
+}
